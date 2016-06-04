5a9fa90343bfdd2b0a32622cf878bebc4aba5bd3
Sayarasin <sayarasin@263.net>
2016-06-04 10:54:03 +0800

    Made the base project

---
+++ JavaPattern/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * This is the main
+ * Created by Sayarasin on 6/4/2016.
+ */
+public class Main {
+
+    public static void main(String [] args) {
+        System.out.println("Hello word");
+    }
+}
