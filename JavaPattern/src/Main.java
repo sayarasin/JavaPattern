69b1e8ce14032a8253b7702c0018ae4c60d75ed1
Sayarasin <sayarasin@263.net>
2016-06-04 15:44:20 +0800

    Create singleton pattern mode

--- JavaPattern/src/Main.java
+++ JavaPattern/src/Main.java
@@ -1,5 +1,6 @@
-import Observer.Subject;
 import Observer.Observer;
+import Observer.Subject;
+import Singleton.Singleton;
 
 /**
  * This is the main
@@ -10,11 +11,23 @@ public class Main {
     private static Subject subject = new Subject();
 
     public static void main(String [] args) {
+
+        // Observer
         Observer observer1 = new Observer();
         subject.register(observer1);
         subject.setValue(10);
         subject.setValue(20);
         subject.unregister(observer1);
         subject.setValue(30);
+
+        //Singleton
+        Singleton singleton = Singleton.getSingleton();
+        System.out.println(singleton.getValue());
+        singleton.setValue(10);
+        System.out.println(singleton.getValue());
+        Singleton singleton2 = Singleton.getSingleton();
+        System.out.println(singleton2.getValue());
+
+
     }
 }
---
+++ JavaPattern/src/Singleton/Singleton.java
@@ -0,0 +1,30 @@
+package Singleton;
+
+/**
+ * This is a singleton implement
+ * Created by Sayarasin on 6/4/2016.
+ */
+public class Singleton {
+
+    private int _value = 0;
+    private static Singleton _signleton = null;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getSingleton() {
+        if(_signleton == null)
+            _signleton = new Singleton();
+
+        return _signleton;
+    }
+
+    public void setValue(int value){
+        _value = value;
+    }
+
+    public int getValue() {
+        return _value;
+    }
+}
